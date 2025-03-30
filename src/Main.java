31c33b3527f6fa06b919a00465a533f139b138e9
Halil <scannerhalil@gmail.com>
2025-03-30 22:26:38 +0300

    Version1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the second number: ");
+        int m = sc.nextInt();
+
+        System.out.println("Sum of two numbers: " + sumOfTwoNumbers(n, m));
+    }
+
+    public static int sumOfTwoNumbers(int n, int m) {
+        int sum = n + m;
+        return sum;
+    }
+}
